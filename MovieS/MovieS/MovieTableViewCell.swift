269eff60454b0c710b5cd967d8a1717c3beaa6c9
Ornella Musso <ornellamusso@yahoo.com>
2025-01-29 10:10:58 +0100

    son las 10:09 am - si se observa he pasado toda la noche trabajando para investigar porque no me resulta exacto al tutorial.

--- MovieS/MovieS/MovieTableViewCell.swift
+++ MovieS/MovieS/MovieTableViewCell.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet var movieTitleLabel: UILabel!
+    @IBOutlet var movieYearLabel: UILabel!
+    @IBOutlet var moviePosterImageView: UIImageView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+      
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -20,4 +24,17 @@ class MovieTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    static let identifier = "MovieTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "MovieTableViewCell", bundle: nil)
+    }
+    
+    func configure(with model: Movie) {
+        self.movieTitleLabel.text = model.Title
+        self.movieYearLabel.text = model.Year
+        let url = model.Poster
+        let data = try! Data(contentsOf: URL(string: url)!)
+        self.moviePosterImageView.image = UIImage(data: data)
+    }
 }
--- MovieS/MovieS/ViewController.swift
+++ MovieS/MovieS/ViewController.swift
@@ -3,84 +3,108 @@
 //  MovieS
 //
 //  Created by Esclava de chofi, lilly y lulu on 29/1/25.
-// UI
+
 // ver este tutorial que es el que realizo paso a paso: https://www.youtube.com/watch?v=mT3OFcui97k
-// Network request
-// tap a cell to see info about de movie
-// custom cell
+
+
 
 
 import UIKit
+
+// tap a cell to see info about de movie
+// custom cell
 
     class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
   
     @IBOutlet var table: UITableView!
     @IBOutlet var field: UITextField!
     
-    var movies = [Movie] ()
+        var movies = [Movie]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.register(MovieTableViewCell.nib(), forCellReuseIdentifier: MovieTableViewCell.identifier)
         table.delegate = self
         table.dataSource = self
-        field.delegate = self 
-        // Do any additional set up after
+        field.delegate = self
     }
     
     // Field
-    
-    func textFieldShouldReturn(_textField: UITextField) ->
-    searchMovies()
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchMovies;()
     return true
-    
     }
-    func searchMovies() {
-    field.resignFirstResponder()
         
-        guard let text = field.text, !text.isEmpty else
+    func searchMovies() {
+        field.resignFirstResponder()
+    }
+        guard; let text = field.text, !text.isEmpty else {
         return
     }
-    URLSession.shared.dataTask(with: URL(string: "http://www.omdbapi.com/?apikey=3aea79ac&s=fast%20and&type=movie")!,
-                               completionHandler: { data, response, error in
+
+    let query = text.replacingOccurrences(of: " ", with: "%20")
+
+        func movies;.removeAll()
+
+    URLSession.shared.dataTask(with: URL(string: "http://www.omdbapi.com/?apikey=3aea79ac&s=\(query)&type=movie")!,
+                        completionHandler: {data, response, error in
         
-        guard let data = data, error == nil else {
-            return
-                  }
-        // Convert
+                        guard let data = data, error == nil else {
+                        return
+                        }
+    
+                        // Convert
+                        var result: MovieResult?
+                        result = try JSONDecoder().decode(MovieResult.self, from: data)
+                        do {
+                        }
+                        catch {
+                        print("error")
+                        }
+                        guard let finalResult = result else {
+                        return
+                        }
         
-        // Updates our movies array
+                        // Updates our movies array
+                        let newMovies = finalResult.Search
+                        self.movies.append(contentsOf:newMovies)
+                                           
+                        // Refresh our table
+                        DispatchQueue.main.async {
+                        self.table.reloadData()
+                        }
         
-        // Refresh our table
-        
-        
-    }).resume()
+                        ).resume()
 
    // Table
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return movies.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    return UITableViewCell ()
+        let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableViewCell.identifier, for: indexPath) as! MovieTableViewCell
+        cell.configure(with: movies[indexPath.row])
+        return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     tableView.deselectRow(at: indexPath, animated: true)
     // Show movie details
     }
 
-struct MovieResult: Codable {
+    }
+                               
+ struct MovieResult: Codable {
     let Search: [Movie]
 }
 
 struct Movie: Codable {
-    let Title: String
-    let Year: String
-    let imdbID: String
-    let _Type: String
-    let Poster: String
-    
-    private enum Codingkeys: String, CodingKey {
-        case Title, Year, imdbID, _Type = "Type", Poster
+        let Title: String
+        let Year: String
+        let imdbID: String
+        let _Type: String
+        let Poster: String
         
-    }
+        private enum Codingkeys: String, CodingKey {
+            case Title, Year, imdbID, _Type = "Type", Poster}
 }
+        }
