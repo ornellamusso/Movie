5d1a8ae9392c20424f6a0170ab9c459f80d7290d
Ornella Musso <ornellamusso@yahoo.com>
2025-01-29 05:42:09 +0100

    continuo haciendo el step by step del tutorial antes mencionado, y me presenta errores en codigos que a el no se lo presenta. Tutorial: https://www.youtube.com/watch?v=mT3OFcui97k

--- MovieS/MovieS/ViewController.swift
+++ MovieS/MovieS/ViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Esclava de chofi, lilly y lulu on 29/1/25.
 // UI
+// ver este tutorial que es el que realizo paso a paso: https://www.youtube.com/watch?v=mT3OFcui97k
 // Network request
 // tap a cell to see info about de movie
 // custom cell
@@ -11,7 +12,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+    class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
   
     @IBOutlet var table: UITableView!
     @IBOutlet var field: UITextField!
@@ -26,29 +27,60 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
         // Do any additional set up after
     }
     
-    //Field
+    // Field
+    
     func textFieldShouldReturn(_textField: UITextField) ->
-    searchMovies ()
+    searchMovies()
     return true
     
     }
     func searchMovies() {
     field.resignFirstResponder()
+        
+        guard let text = field.text, !text.isEmpty else
+        return
+    }
+    URLSession.shared.dataTask(with: URL(string: "http://www.omdbapi.com/?apikey=3aea79ac&s=fast%20and&type=movie")!,
+                               completionHandler: { data, response, error in
+        
+        guard let data = data, error == nil else {
+            return
+                  }
+        // Convert
+        
+        // Updates our movies array
+        
+        // Refresh our table
+        
+        
+    }).resume()
+
+   // Table
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int {
+    return movies.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell ()
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    // Show movie details
+    }
+
+struct MovieResult: Codable {
+    let Search: [Movie]
+}
+
+struct Movie: Codable {
+    let Title: String
+    let Year: String
+    let imdbID: String
+    let _Type: String
+    let Poster: String
     
- }
-   //Table
-
-func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int {
-  return movies.count
-}
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-   return UITableViewCell ()
- }
-func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-  tableView.deselectRow(at: indexPath, animated: true)
-  //Show movie details
-}
-
-struct Movie{
-
+    private enum Codingkeys: String, CodingKey {
+        case Title, Year, imdbID, _Type = "Type", Poster
+        
+    }
 }
