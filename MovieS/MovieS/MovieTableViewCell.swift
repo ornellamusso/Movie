798740fde641dd8b6a73c019c23853218c5362b5
Ornella Musso <ornellamusso@yahoo.com>
2025-01-29 07:29:06 +0100

    he logrado no tener error en la cell, (2 de cocoa) MovieTableViewCell, continuo con errores en el ViewController que seguire revisando.

---
+++ MovieS/MovieS/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  MovieS
+//
+//  Created by Esclava de chofi, lilly y lulu on 29/1/25.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
